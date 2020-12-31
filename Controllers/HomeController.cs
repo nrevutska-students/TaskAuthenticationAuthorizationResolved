5a033a57aa0bee8261a74ede252863043f6ca3dc
Nataliya Revutska <nrevu@softserveinc.com>
2020-12-31 12:49:57 +0200

    removed redundant authorize arrtibute

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace TaskAuthenticationAuthorization.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        [Authorize]
         public IActionResult Index()
         {
             return View();
